c3c49ea1c514e54406c0d7c4a14a1fdccd9f3400
Aleh <yem1403@gmail.com>
2019-09-04 22:30:54 +0300

    Added AppRunner class.

--- src/by/epam/ayem/module4/AppRunner.java
+++ src/by/epam/ayem/module4/AppRunner.java
@@ -6,41 +6,8 @@ package by.epam.ayem.module4;
 номер которого введен пользователем. Добавьте возможность сортировки массива по пункту назначения,
 причем поезда с одинаковыми пунктами назначения должны быть упорядоченны по времени отправления.*/
 
-public class Train {
-
-    private String destination;
-    private int trainNumber;
-    private String departureTime;
-
-    Train(String destination, int trainNumber, String departureTime) {
-        this.destination = destination;
-        this.trainNumber = trainNumber;
-        this.departureTime = departureTime;
-    }
-
-    public String getDestination() {
-        return destination;
-    }
-
-    public void setDestination(String destination) {
-        this.destination = destination;
-    }
-
-    public int getTrainNumber() {
-        return trainNumber;
-    }
-
-    public void setTrainNumber(int trainNumber) {
-        this.trainNumber = trainNumber;
-    }
-
-    public String getDepartureTime() {
-        return departureTime;
-    }
-
-    public void setDepartureTime(String departureTime) {
-        this.departureTime = departureTime;
+public class AppRunner {
+    public static void main(String[] args) {
+        new TrainMain().run();
     }
 }
-
-
--- src/by/epam/ayem/module4/TrainMain.java
+++ src/by/epam/ayem/module4/TrainMain.java
@@ -1,24 +1,23 @@
 package by.epam.ayem.module4;
-/*4. Создайте класс task4train, содержащий поля: название пункта назначения, номер поезда, время отправления.
-Создайте данные в массиве из пяти элементов типа task4train, добавьте возможность сортировки элементов массива
-по номерам поездов. Добавьте возможность вывода информации о поезде, номер которого введен пользователем.
-Добавьте возможность сортировки массива по пункту назначения, причем поезда с одинаковыми пунктами назначения
-должны быть упорядоченны по времени отправления.*/
+
+/*4. Создайте класс Train, содержащий поля: название пункта назначения, номер поезда, время
+отправления. Создайте данные в массиве из пяти элементов типа Train, добавьте возможность
+сортировки элементов массива по номерам поездов. Добавьте возможность вывода информации о поезде,
+номер которого введен пользователем. Добавьте возможность сортировки массива по пункту назначения,
+причем поезда с одинаковыми пунктами назначения должны быть упорядоченны по времени отправления.*/
 
 import java.util.Scanner;
 
 public class TrainMain {
 
-    public static void main(String[] args) {
+    public void run() {
 
-
-        Train[] trains = new Train[6];
+        Train[] trains = new Train[5];
         trains[0] = new Train("Berlin", 22786, "10:30");
         trains[1] = new Train("Warsaw", 77886, "06:54");
         trains[2] = new Train("Berlin", 33861, "08:05");
         trains[3] = new Train("Minsk", 33754, "11:30");
         trains[4] = new Train("Berlin", 45268, "12:00");
-        trains[5] = new Train("Minsk", 22146, "11:25");
 
         printTrains(trains);
         sortByTrainNumber(trains);
@@ -31,15 +30,14 @@ public class TrainMain {
         printTrains(trains);
 
         printTrainInfo(trains);
-
     }
 
-    private static void printTrainInfo(Train[] trains) {
+    private void printTrainInfo(Train[] trains) {
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println();
-        System.out.println("Enter a task4train number: ");
+        System.out.println("Enter a train number: ");
 
         while (!sc.hasNextInt()) {
             sc.next();
@@ -56,10 +54,10 @@ public class TrainMain {
 
         }
 
-        System.out.println("The task4train has not found.");
+        System.out.println("The train has not found.");
     }
 
-    private static void sortByDestination(Train[] trains) {
+    private void sortByDestination(Train[] trains) {
 
         boolean quit = false;
 
@@ -83,7 +81,7 @@ public class TrainMain {
         }
     }
 
-    private static boolean sortByDepartureTime(Train[] trains, int i) {
+    private boolean sortByDepartureTime(Train[] trains, int i) {
 
         String[] time1 = trains[i].getDepartureTime().split(":");
         String[] time2 = trains[i - 1].getDepartureTime().split(":");
@@ -102,7 +100,7 @@ public class TrainMain {
         return false;
     }
 
-    private static void sortByTrainNumber(Train[] trains) {
+    private void sortByTrainNumber(Train[] trains) {
 
         boolean quit = false;
 
@@ -122,16 +120,16 @@ public class TrainMain {
         }
     }
 
-    private static void printTrains(Train train) {
+    private void printTrains(Train train) {
 
-        System.out.println("task4train #" + train.getTrainNumber() + " to " +
+        System.out.println("Train #" + train.getTrainNumber() + " to " +
                 train.getDestination() + " departs at " + train.getDepartureTime());
     }
 
-    private static void printTrains(Train[] trains) {
+    private void printTrains(Train[] trains) {
 
         for (Train train : trains) {
-            System.out.println("task4train #" + train.getTrainNumber() + " to " +
+            System.out.println("Train #" + train.getTrainNumber() + " to " +
                     train.getDestination() + " departs at " + train.getDepartureTime());
         }
     }
